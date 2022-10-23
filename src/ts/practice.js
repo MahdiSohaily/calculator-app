37e249c06e4f7e2d22db72d2d0521931d140d273
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-23 08:06:52 +0430

    Practice primitive types in typescript

---
+++ src/ts/practice.js
@@ -0,0 +1,10 @@
+var userName = 'mahdi';
+var age = 25;
+var isMarried = false;
+var socialMedia = ['facebook', 'LinkedIn', 'Twitter'];
+var profile = {
+    name: "Mahdi",
+    age: 25,
+    isMarried: false,
+    socialMedia: ['Facebook', 'LinkedIn', 'Twitter']
+};
---
+++ src/ts/practice.ts
@@ -0,0 +1,15 @@
+const userName: string = 'mahdi';
+const age: number = 25;
+const isMarried: boolean = false;
+const socialMedia: string[] = ['facebook', 'LinkedIn', 'Twitter'];
+const profile: {
+  name: string;
+  age: number;
+  isMarried: boolean;
+  socialMedia: string[];
+} = {
+    name: "Mahdi",
+    age: 25,
+    isMarried: false,
+    socialMedia: ['Facebook', 'LinkedIn', 'Twitter'],
+};
