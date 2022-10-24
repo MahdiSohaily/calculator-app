71aca44eb8757c8e70139f8c545d59ec5b867900
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-24 17:29:36 +0430

    Add them switcher initial scripts

--- public/js/modules/themeController.js
+++ public/js/modules/themeController.js
@@ -1,5 +1,18 @@
 "use strict";
 const track = document.querySelector('.track');
-track.addEventListener('click', () => {
-    alert('clicked');
+track.addEventListener('click', (e) => {
+    const elem = e.target;
+    const position = elem.getAttribute('data-position');
+    switch (position) {
+        case '1': {
+            break;
+        }
+        case '2': {
+            break;
+        }
+        case '3': {
+            break;
+        }
+        default:
+    }
 });
--- src/modules/themeController.ts
+++ src/modules/themeController.ts
@@ -1,6 +1,19 @@
 const track = document.querySelector('.track') as HTMLDivElement;
 
-track.addEventListener('click', () => {
-  alert('clicked');
-});
+track.addEventListener('click', (e: Event) => {
+  const elem = e.target as HTMLDivElement;
+  const position: string = elem.getAttribute('data-position')!;
 
+  switch (position) {
+    case '1': {
+        break;
+    }
+    case '2': {
+        break;
+    }
+    case '3': {
+      break;  
+    }
+    default :
+  }
+});
