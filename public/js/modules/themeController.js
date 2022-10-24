fdcab9b8d9e41a4d588ee8c23a7cebbd91d63eb3
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-10-24 18:31:16 +0430

    Add comment to the code to be more descriptive

--- public/js/modules/themeController.js
+++ public/js/modules/themeController.js
@@ -1,32 +1,66 @@
 "use strict";
+// DOM Reference to the theme changer element
 const track = document.querySelector('.track');
+/**
+ * Event Listener attached to the div element
+ * by the .track class to listen for click event
+ * in order to change displaying theme
+ */
 track.addEventListener('click', (e) => {
     const elem = e.target;
     const theme = elem.getAttribute('data-display');
     switchTheme(elem, theme);
 });
+/**
+ * Function receives following parameters
+ * @param elem
+ * @param theme
+ * check for the current theme and change
+ * the theme to the next theme predefined
+ */
 const switchTheme = (elem, theme) => {
+    const root = document.querySelector(':root');
     switch (theme) {
         case 'dark': {
             elem.style.left = '27px';
-            elem.setAttribute('data-position', 'light');
+            elem.setAttribute('data-display', 'light');
+            root.setAttribute('data-color-scheme', 'light');
             break;
         }
         case 'light': {
             elem.style.left = '51px';
-            elem.setAttribute('data-position', 'purple');
+            changeAttribute(elem, 'data-display', 'purple');
+            changeAttribute(root, 'data-color-scheme', 'purple');
             break;
         }
         case 'purple': {
             elem.style.left = '3px';
-            elem.setAttribute('data-position', 'dark');
+            changeAttribute(elem, 'data-display', 'dark');
+            changeAttribute(root, 'data-color-scheme', 'dark');
             break;
         }
         default:
             elem.style.left = '3px';
-            elem.setAttribute('data-position', 'dark');
+            changeAttribute(elem, 'data-display', 'dark');
+            changeAttribute(root, 'data-color-scheme', 'dark');
     }
 };
-function updateLocalStorage(theme, position) {
-    localStorage.setItem('theme', JSON.stringify({ theme, position }));
+/**
+ * Update the local storage @calcTheme item value
+ * with the @param theme  value and update local storage
+ * for later usage
+ */
+function updateLocalStorage(theme) {
+    localStorage.setItem('calcTheme', theme);
+}
+/**
+ * This function receive three following parameters
+ * @param elem
+ * @param attr
+ * @param value
+ * then change specified element's attribute with
+ * provided value for attribute.
+ */
+function changeAttribute(elem, attr, value) {
+    elem.setAttribute(attr, value);
 }
