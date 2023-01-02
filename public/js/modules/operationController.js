c6a026fd9f26c08c6890d2f6c4040b3e989553e2
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-01-02 19:15:58 +0430

    Add function to add fraction part

--- public/js/modules/operationController.js
+++ public/js/modules/operationController.js
@@ -4,15 +4,31 @@ let operation = null;
 let num2 = null;
 
 function isNumber(item) {
-  return !!item.match(/[0-9]+/);
+  return !!item.match(/[0-9.]+/);
 }
 
 const keys = document.querySelectorAll(".key-text");
+const screen = document.getElementById("result");
+screen.innerText = num1 ? num1 : 0;
+
 keys.forEach((key) => {
   key.addEventListener("click", (e) => {
     const value = e.target.innerText;
-    if (isNumber()) {
+    if (isNumber(value)) {
+      if (null === operation) {
+        num1 !== null ? (num1 += value) : (num1 = value);
+        console.log(Number(num1));
+      } else {
+        num2 !== null ? (num2 += value) : (num2 = value);
+        console.log(num2);
+      }
     } else {
+      operation = value;
+      switch (value) {
+        case "DEL":
+          break;
+        case "+":
+      }
     }
   });
 });
