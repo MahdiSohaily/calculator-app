21960e8df32d3d19a0d42d874ca0febe84ae297d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-01-03 18:00:58 +0430

    Add show result on = button

--- public/js/modules/operationController.js
+++ public/js/modules/operationController.js
@@ -36,54 +36,56 @@ keys.forEach((key) => {
         if (value !== "." || (value === "." && !notFractional(num2))) {
           num2 !== null && num2 !== 0 ? (num2 += value) : (num2 = value);
         }
-        switch (operation) {
-          case "+": {
-            const result = Number(num1) + Number(num2);
-            num1 = result;
-            showResult(result);
-            cleanup();
-            break;
-          }
-          case "-": {
-            const result = Number(num1) - Number(num2);
-            num1 = result;
-            showResult(result);
-            cleanup();
-            break;
-          }
-          case "*": {
-            const result = Number(num1) * Number(num2);
-            num1 = result;
-            showResult(result);
-            cleanup();
-            break;
-          }
-          case "/": {
-            try {
-              const result = Number(num1) / Number(num2);
-              num1 = result;
-              showResult(result);
-              cleanup();
-            } catch (error) {
-              showResult("con't divide my 0.");
-            }
-            break;
-          }
-          case "RESET":
-          case "DEL": {
-            num1 = null;
-            showResult();
-            cleanup();
-            break;
-          }
-        }
       }
     } else {
       operation = value;
       switch (value) {
         case "DEL":
           break;
-        case "+":
+        case "=":
+          console.log('here');
+          switch (operation) {
+            case "+": {
+              console.log('addition');
+              const result = Number(num1) + Number(num2);
+              num1 = result;
+              showResult(result);
+              cleanup();
+              break;
+            }
+            case "-": {
+              const result = Number(num1) - Number(num2);
+              num1 = result;
+              showResult(result);
+              cleanup();
+              break;
+            }
+            case "*": {
+              const result = Number(num1) * Number(num2);
+              num1 = result;
+              showResult(result);
+              cleanup();
+              break;
+            }
+            case "/": {
+              try {
+                const result = Number(num1) / Number(num2);
+                num1 = result;
+                showResult(result);
+                cleanup();
+              } catch (error) {
+                showResult("con't divide my 0.");
+              }
+              break;
+            }
+            case "RESET":
+            case "DEL": {
+              num1 = null;
+              showResult();
+              cleanup();
+              break;
+            }
+          }
       }
     }
   });
